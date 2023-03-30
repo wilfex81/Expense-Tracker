791b607923fd0dd884a6d10014952c443310d47d
wilfex <wilfex81@gmail.com>
2023-03-30 23:26:04 +0300

    added models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
-# Create your models here.
+
+
+class Account(models.Model):
+    LIST = [
+        ('Account Options',(
+                    ('M-Pesa', 'M-Pesa'),
+                    ('Bank', 'Bank'),
+                    ('Credit_Card', 'Credit_Card'),
+                )
+            
+        ),
+        
+    ]
+
+    TYPE_OF_INCOME = [
+        ('Account Options',(
+                    ('salary', 'salary'),
+                    ('saving', 'saving'),
+                    ('investment', 'investment'),
+                )
+        ), 
+    ]
+    account_name = models.CharField(max_length=50, choices=LIST)
+    saving_list = models.CharField(max_length=50, choices=TYPE_OF_INCOME)
+    deposited_amount = models.DecimalField(max_digits=15, decimal_places=2)
+    deposited_date = models.DateField()
+
+
+    def __str__(self)-> str:
+        return f"{self.account_name}: {self.deposited_amount}"
+
+
+class BudgetCategorie(models.Model):
+    title = models.CharField(max_length=100, help_text='e.g. Utilities, groceries, etc.')
+    budget = models.DecimalField(max_digits=15, decimal_places=2)
+    date_budgeted = models.DateField()
+
+    def __str__(self)-> str:
+        return f"{self.title}: {self.budget}"
+    
